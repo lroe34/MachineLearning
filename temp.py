40e47c264b2cecdf815b023253db6ad24dfda311
lroe34 <101296666+lroe34@users.noreply.github.com>
2024-04-09 22:43:55 -0500

    commit

--- temp.py
+++ temp.py
@@ -1,10 +1,5 @@
-# import iris data, do kmeans with different scalars
-from sklearn.cluster import KMeans
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.datasets import load_iris
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import RobustScaler
-from sklearn.preprocessing import MaxAbsScaler
 
 import numpy as np
 import pandas as pd
@@ -14,36 +9,36 @@ from scipy.stats import mode
 iris = load_iris()
 X = iris.data
 y = iris.target
-scalers = [MinMaxScaler(), StandardScaler(), RobustScaler(), MaxAbsScaler()]
-
-def resolve_masking(y_pred, y_true):
-    pred_labels = np.zeros_like(y_true)
-    for i in range(len(np.unique(y_pred))):
-        mask = (y_pred == i)
-        pred_labels[mask] = mode(y_true[mask], keepdims=True)[0]
-    return pred_labels
-
-for scaler in scalers:
-    X_scaled = scaler.fit_transform(X)
-    kmeans = KMeans(n_clusters=3, random_state=42)
-    kmeans.fit_predict(X_scaled)
-    y_pred = kmeans.labels_
-    y_true = y
-    y_pred = resolve_masking(y_pred, y_true)
-    print(accuracy_score(y_true, y_pred))
 
 
-def computeLearningHardIndex(acc, vcr):
-    return (1 - acc) * vcr
+def test_knn(metric='cosine', n_neighbors=3):
+    knn = KNeighborsClassifier(n_neighbors=n_neighbors, metric=metric)
+    knn.fit(X, y)
+    y_pred = knn.predict(X)
+    return accuracy_score(y, y_pred)
 
-def VCR (data):
-    U, s, V = np.linalg.svd(data)
-    vcr = s[0] / np.sum(s)
-    return vcr
+print("cosine: " + str(test_knn()))
 
-kmeans_acc_maxabs = accuracy_score(y, y_pred)
-vcr_maxabs = VCR(X)
-LHI_maxabs = computeLearningHardIndex(kmeans_acc_maxabs, vcr_maxabs)
+import numpy as np
+import matplotlib.pyplot as plt
+from mlp_toolkit.mplot3d import Axes3D
 
+X = np.linspace(-5, 5, 100)
+Y = np.linspace(-5, 5, 100)
 
+X, Y = np.meshgrid(X, Y)
 
+Z = X**2 + Y**2
+
+fig = plt.figure(figsize=(8, 6))
+ax = fig.add_subplot(111, projection='3d')
+
+surf = ax.plot_surface(X, Y, Z, cmap='viridis')
+
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+
+fig.colorbar(surf, ax=ax)
+
+plt.show()
